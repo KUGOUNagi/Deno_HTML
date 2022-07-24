93d0111891cd19223eab20dedc9d511b0445b474
KUGOUNagi <e8896@g.maizuru-ct.ac.jp>
2022-07-24 16:17:47 +0900

    ひらがな以外禁止

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import { serveDir } from "https://deno.land/std@0.138.0/http/file_server.ts";
 let previousWord = "しりとり";
 let wordFile= ['ろしあ','かい','うもう','うえ','かお'];
 let myArray=[];
+let regexp = /^[\u{3000}-\u{301C}\u{3041}-\u{3093}\u{309B}-\u{309E}]+$/mu;
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -65,6 +66,17 @@ serve(async (req) => {
     ) {
 
       return new Response("「ん」と言いました。あなたの負けです。", { status: 400 });
+
+    }
+    if (
+
+      nextWord.length > 0 &&
+
+      regexp.test(nextWord) !==true
+
+    ) {
+
+      return new Response("ひらがな以外が混ざっています。", { status: 400 });
 
     }
 
