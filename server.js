c73c53f6201f42e22cbeba9bf2d3c1120a768385
KUGOUNagi <e8896@g.maizuru-ct.ac.jp>
2022-07-28 16:18:25 +0900

    check

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ import { serveDir } from "https://deno.land/std@0.138.0/http/file_server.ts";
 
 
 let previousWord = "しりとり";
-const wordFile = FileReader.readAsText(Blob("./public/text.txt" , {type:"text/plain"}));
+let wordFile = ['ろしあ','かい','うもう','うえ','かお'];
 let myArray=[];
 let regexp = /^[\u{3041}-\u{3093}\u{309B}-\u{309E}\u{30FC}]+$/mu;
 //['ろしあ','かい','うもう','うえ','かお'];
-FileReader.readAsText(Blob("./public/text.txt" , {type:"text/plain"}));
+//FileReader.readAsText(Blob("./public/text.txt" , {type:"text/plain"}));
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
